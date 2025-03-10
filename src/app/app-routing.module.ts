525fa25fb293c8948a845a9eae1c0a908752b694
daniel.ruiz-gil@iesruizgijon.com <tuemail@example.com>
2025-03-10 12:12:36 +0100

    Añadido las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BienvenidaComponent} from './bienvenida/bienvenida.component';
+import {BusquedasComponent} from './busquedas/busquedas.component';
+import {AcercaDeComponent} from './acerca-de/acerca-de.component';
+import {PaginaNoEncontrada404Component} from './pagina-no-encontrada404/pagina-no-encontrada404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BienvenidaComponent },
+  { path: 'busquedas', component: BusquedasComponent },
+  { path: 'acerca-de', component: AcercaDeComponent },
+  { path: '**', component: PaginaNoEncontrada404Component }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
