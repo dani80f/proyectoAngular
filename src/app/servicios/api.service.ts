995673195250172963093cb52e954028e5609827
daniel.ruiz-gil@iesruizgijon.com <tuemail@example.com>
2025-03-10 12:52:37 +0100

    Servicio actualizado para la API

--- src/app/servicios/api.service.ts
+++ src/app/servicios/api.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  private apiUrl = 'https://api.football-data.org/v4';
+  private apiKey = 'c07636e635934f09b97f94a0ae70feb6';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+
+  getPartidosEuro(): Observable<any> {
+
+    const headers = new HttpHeaders({
+
+      'X-Auth-Token': this.apiKey,
+
+
+    });
+
+    const url = `${this.apiUrl}/competitions/EC/matches`;
+
+    return this.http.get(url, { headers });
+  }
+
+
 }
