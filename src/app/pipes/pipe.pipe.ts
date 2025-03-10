6a3002d237594004bc5374350e5af7afe84792e1
daniel.ruiz-gil@iesruizgijon.com <tuemail@example.com>
2025-03-10 18:26:08 +0100

    Añadido bootstrap e implementacion del pipe

--- src/app/pipes/pipe.pipe.ts
+++ src/app/pipes/pipe.pipe.ts
@@ -7,8 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PipePipe implements PipeTransform {
 
   transform(value: string): string {
-
-    return value.toUpperCase();
+    return value ? value.toUpperCase() : '';
   }
 
 }
