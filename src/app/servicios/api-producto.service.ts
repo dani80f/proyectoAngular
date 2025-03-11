39f35aa125e20d8abe30a9bdaf5903784cad2780
daniel.ruiz-gil@iesruizgijon.com <tuemail@example.com>
2025-03-11 11:15:33 +0100

    Implementado logica a la api producto

--- src/app/servicios/api-producto.service.ts
+++ src/app/servicios/api-producto.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiProductoService {
 
-  constructor() { }
+  private url="https://67d0073d823da0212a844849.mockapi.io/producto";
+
+  constructor( private http: HttpClient) {
+
+
+  }
+
+  getProductos(): Observable<any[]> {
+
+    return this.http.get<any[]>(this.url);
+
+  }
+
+
+  getProducto(id: string): Observable<any> {
+
+    return this.http.get<any>(`${this.url}/${id}`);
+  }
+
+  createProducto(post: any): Observable<any> {
+    return this.http.post<any>(this.url, post);
+
+  }
+
+  updateProducto(id: string, post: any): Observable<any> {
+    return this.http.put<any>(`${this.url}/${id}`, post);
+
+  }
+
+
+
+  deleteProducto(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.url}/${id}`);
+  }
+
 }
